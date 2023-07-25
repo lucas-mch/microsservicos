39c8bda6ad32c6fe05d986ae785c28b0bf0d8230
Lucas Machado <lcsmachaado@gmail.com>
2023-07-25 11:42:29 -0300

    User -> Routes layer

---
+++ auth/src/modules/user/routes/UserRoutes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import userController from '../controller/userController.js';
+
+const router = new Router();
+
+router.get('/api/user/email/:email', userController.findByEmail);
+router.post('/api/user/auth', userController.getAccessToken);
+
+export default router;
